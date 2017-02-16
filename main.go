07abe4ffcdbd2c1c44de8b285a00e42891315379
Maxime Simon <simon.maxime@gmail.com>
2017-02-16 23:05:06 +0100

    feat: new `folders` endpoint to retrieve folders

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"path/filepath"
+	"strings"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
@@ -15,31 +16,46 @@ var ext = flag.String("ext", ".*", "file extension to filter")
 
 func main() {
 	flag.Parse()
-	http.Handle("/", http.HandlerFunc(lsRepo))
+	http.Handle("/files", http.HandlerFunc(lsFiles))
+	http.Handle("/folders", http.HandlerFunc(lsFolders))
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
 
-func lsRepo(w http.ResponseWriter, req *http.Request) {
+func lsFiles(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(ls(*repo)); err != nil {
+	if err := json.NewEncoder(w).Encode(ls(*repo, false)); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func ls(rep string) []string {
+func lsFolders(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(ls(*repo, true)); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ls(rep string, onlyFolders bool) []string {
 	var result []string
 	files, err := ioutil.ReadDir(rep)
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, file := range files {
+		if strings.HasPrefix(file.Name(), ".") {
+			// ignore hidden files
+			continue
+		}
 		if file.IsDir() {
+			if onlyFolders {
+				result = append(result, filepath.Clean(rep+"/"+file.Name()))
+			}
 			// recurse on directory
-			result = append(result, ls(rep+"/"+file.Name())...)
-		} else if *ext == ".*" || filepath.Ext(file.Name()) == *ext {
+			result = append(result, ls(rep+"/"+file.Name(), onlyFolders)...)
+		} else if !onlyFolders && (*ext == ".*" || filepath.Ext(file.Name()) == *ext) {
 			// only return files of given extension
 			result = append(result, filepath.Clean(rep+"/"+file.Name()))
 		}
